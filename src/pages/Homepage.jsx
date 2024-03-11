60a11f22164a75aa6eb4491ab2fba3f24b86e085
anonymousboy67 <aashishad67@gmail.com>
2024-03-11 21:33:41 +0545

    Home Page

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import PageNav from "../components/PageNav";
+
+
+export default function Homepage(){
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <PageNav/>
+    
+    </div>
+  )
+};
+
+
